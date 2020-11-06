104e67c92086f6b6f85ebe076db11fa71b62ae77
Tim Kante <tim@timkante.dev>
2020-11-06 21:32:18 +0100

    :alembic: testing githubs pom handling

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    maven
     application
     kotlin("jvm") version "1.4.0"
     kotlin("plugin.serialization") version "1.4.0"
@@ -14,33 +15,55 @@ allprojects {
     group = "kotlin.dev.timkante"
     version = "1.0-SNAPSHOT"
 
-    this.allprojects
-        .filterNot {
-            it.name == "platform"
-        }
-        .forEach {
-            it.apply {
-                plugin("org.jetbrains.kotlin.jvm")
-                plugin("org.jetbrains.kotlin.plugin.serialization")
-
-                kotlin.sourceSets["main"].kotlin.srcDirs("src")
-                kotlin.sourceSets["test"].kotlin.srcDirs("test")
-
-                sourceSets["main"].resources.srcDirs("resources")
-                sourceSets["test"].resources.srcDirs("testresources")
-
-                tasks {
-                    withType<KotlinCompile> {
-                        kotlinOptions {
-                            jvmTarget = "14"
+    task("generatePom") {
+        group = "maven"
+        description = "generates a maven pom.xml file based on the build.gradle.kts file"
+        doLast {
+            maven.pom {
+                project {
+                    groupId = this@task.group as String
+                    artifactId = artifactId
+                    version = version
+                    withGroovyBuilder {
+                        "modules" {
+                            subprojects.map {
+                                "module"(it.name)
+                            }
                         }
+                        "inceptionYear"("2042")
                     }
+                }
+            }.writeTo("pom.xml")
+        }
+    }
+
+    if (this.name != "platform") {
+        apply {
+            plugin("org.jetbrains.kotlin.jvm")
+            plugin("org.jetbrains.kotlin.plugin.serialization")
+        }
+
+        kotlin.sourceSets["main"].kotlin.srcDirs("src")
+        kotlin.sourceSets["test"].kotlin.srcDirs("test")
+
+        sourceSets["main"].resources.srcDirs("resources")
+        sourceSets["test"].resources.srcDirs("testresources")
+
+        tasks {
+            build {
+                dependsOn("generatePom")
+            }
+
+            withType<KotlinCompile> {
+                kotlinOptions {
+                    jvmTarget = "14"
                 }
             }
         }
 
-    repositories {
-        mavenCentral()
+        repositories {
+            mavenCentral()
+        }
     }
 }
 
